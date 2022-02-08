4e70f3a93c581210978687f0573f41923227298e
João Vitor <jovi.oli04@gmail.com>
2022-02-08 14:03:17 -0300

    Projeto shopping_cart removido

--- shopping/bin/shopping.dart
+++ shopping/bin/shopping.dart
@@ -1,9 +1,3 @@
-import 'controllers/cartcontroller.dart';
-import 'interfaces/iclient.dart';
-import 'models/cart.dart';
-import 'models/client.dart';
-import 'models/product.dart';
-import 'observers/cartobserver.dart';
 
 void main(List<String> arguments) {
 }
